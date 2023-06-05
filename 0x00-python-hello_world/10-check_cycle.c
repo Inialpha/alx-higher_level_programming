d81533d21cc566f7e945471749dea9ebee62a435
Initech <inimfonebong001@gmail.com>
2023-06-05 04:11:52 -0700

    task 0 to 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if linked list has a cycle
+ * @list: pointer to the begining of linked list
+ * Return: 0 if no cycle 1 if cycle exits
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	slow = fast = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			break;
+	}
+	if (fast == NULL)
+		return (0);
+
+	return (1);
+}
