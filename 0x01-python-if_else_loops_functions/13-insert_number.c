b1f0d029eaa947461acd533db975b3ef7ece90d0
Initech <inimfonebong001@gmail.com>
2023-06-06 09:31:37 -0700

    Attempt all mandatory tasks

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * get_index - gets the index
+ * @head: head of list
+ * @num: number to be inserted
+ * Return: index
+ */
+
+int get_index(listint_t *head, int num)
+{
+	listint_t *temp = head;
+	int i = 1;
+
+	while (temp->next != NULL && temp->n < num)
+	{
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
+
+/**
+ * insert_node - insert node in a sorted LL
+ * @head: the pointer to the first node in LL
+ * @number: data to be inserted
+ * Return: address of new node
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *new;
+	int index, i;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	else
+	{
+		index = get_index(*head, number);
+		temp = *head;
+		for (i = 1; i < index - 1; i++)
+			temp = temp->next;
+		new->next = temp->next;
+		temp->next = new;
+	}
+	return (new);
+}
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,6 @@
+# 0x01-python-if_else_loops_functions
+
+In this project i am learning how to use python:
+## variables
+## loops
+## function.
