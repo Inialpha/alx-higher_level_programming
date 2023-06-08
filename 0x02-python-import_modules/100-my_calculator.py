cfabb2ce9f50e2e4596ecee870fc59c7ce16eeea
Initech <inimfonebong001@gmail.com>
2023-06-08 09:44:07 -0700

    fix if else block

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
     argv = sys.argv[1:]
     if len(argv) != 3:
         print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
-        a = int(argv[0])
-        b = int(argv[2])
-        op = argv[1]
+    a = int(argv[0])
+    b = int(argv[2])
+    op = argv[1]
 
-        if op == "+":
-            print("{} {} {} {} {}" .format(a, "+", b, "=", add(a, b)))
+    if op == "+":
+        print("{} + {} = {}" .format(a, b, add(a, b)))
 
-        elif op == "-":
-            print("{} {} {} {} {}" .format(a, "_", b, "=", sub(a, b)))
+    elif op == "-":
+        print("{} - {} = {}" .format(a, b, sub(a, b)))
 
-        elif op == "*":
-            print("{} {} {} {} {}" .format(a, "*", b, "=", mul(a, b)))
+    elif op == "*":
+        print("{} {} {} {} {}" .format(a, "*", b, "=", mul(a, b)))
 
-        elif op == "/":
-            print("{} {} {} {} {}" .format(a, "/", b, "=", div(a, b)))
+    elif op == "/":
+        print("{} {} {} {} {}" .format(a, "/", b, "=", div(a, b)))
 
-        else:
-            print("{}".format
+    else:
+        print("{}".format
                   ("Unknown operator. Available operators: +, -, * and /"))
-            exit(1)
+        exit(1)
