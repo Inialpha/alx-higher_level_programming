22ff161cfcccbb90eb9f0e853671535be58811ea
Inialpha <inimfonebong001@gmail.com>
2023-06-08 14:14:14 +0000

    python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+	import sys
+	from calculator_1 import add, sub, mul, div
+	argv = sys.argv[1:]
+	if len(argv) != 3:
+		print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+		exit(1)
+	a = int(argv[0])
+	b = int(argv[2])
+	op = argv[1]
+
+	if op == "+":
+		print("{} {} {} {} {}" .format(a, "+", b, "=", add(a, b)))
+
+	elif op == "-":
+	        print("{} {} {} {} {}" .format(a, "_", b, "=", sub(a, b)))
+
+	elif op == "*":
+	        print("{} {} {} {} {}" .format(a, "*", b, "=", mul(a, b)))
+
+	elif op == "/":
+	        print("{} {} {} {} {}" .format(a, "/", b, "=", div(a, b)))
+
+	else:
+		print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+		exit(1)
