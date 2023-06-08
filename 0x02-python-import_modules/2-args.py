786ae8f34543bb3de6f69a0998a76b821ec42df7
Initech <inimfonebong001@gmail.com>
2023-06-08 08:31:32 -0700

    update indentation

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-	import sys
-	argv = sys.argv[1:]
-	len = len(argv)
-	if len == 0:
-		print("{}".format("0 arguments."))
-	else:
-		print("{}: {}".format(len, "arguments:" if len > 1 else "argument."))
-		for i in range(len):
-			pos = i + 1
-			print("{}: {}".format(pos, argv[i]))
+    import sys
+    argv = sys.argv[1:]
+    len = len(argv)
+    if len == 0:
+        print("{}".format("0 arguments."))
+    else:
+        print("{}: {}".format(len, "arguments:" if len > 1 else "argument."))
+        for i in range(len):
+            pos = i + 1
+            print("{}: {}".format(pos, argv[i]))
