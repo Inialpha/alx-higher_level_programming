173da084ac408bf3147e468db23422aa6d3370bd
Initech <inimfonebong001@gmail.com>
2023-06-13 09:22:11 -0700

    updates

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,4 @@
-#include <python.h>
+#include <Python.h>
 
 /**
  * print_python_list_info - prints information about python list
@@ -7,19 +7,19 @@
 
 void print_python_list_info(PyObject *p)
 {
-	pyListObject *list = (pyListObject *)p;
-	pyObject obj;
+	PyListObject *list = (PyListObject *)p;
+	PyObject obj;
 
 	int i, size, num_alloc;
 
 	num_alloc = list->allocated;
-	size = py_SIZE(p);
+	size = Py_SIZE(p);
 	printf("[*] Size of the Python List = %d", size);
 	printf("[*] Allocated = %d", num_alloc);
 
 	for (i = 0; i < size; i++)
 	{
-		obj = pyList_GetItem(p, i);
-		printf("Element %d: %s\n", py_TYPE(obj)->tp_name);
+		obj = PyList_GetItem(p, i);
+		printf("Element %d: %s\n", Py_TYPE(obj)->tp_name);
 	}
 }
