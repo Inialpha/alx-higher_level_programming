2f1212c5dab1b1dd2c1e7dc84fcbaa3e03bbe8e9
Initech <inimfonebong001@gmail.com>
2023-06-14 02:32:15 -0700

    add task 2 to task 11

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        print("{} : {}".format(key, a_dictionary[key]))
+
+
+#    for key, value in sorted(a_dictionary.items()):
+#        new_dict = key
+ #       print("{} : {}".format(key, value))
+
