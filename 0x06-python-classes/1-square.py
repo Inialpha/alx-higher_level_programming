4f15ce893d7d45f5f21c9321da07188e11bb5f1a
Initech <inimfonebong001@gmail.com>
2023-06-27 09:41:41 -0700

    Write a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" defines a Square class"""
+
+
+class Square:
+    """defines attributes of the Square"""
+
+    def __init__(self, size):
+        """initializes the square"""
+        self.__size = size
