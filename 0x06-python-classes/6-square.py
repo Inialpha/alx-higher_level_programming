15e11c3866f46896dd51395c5e30948be565bdda
Initech <inimfonebong001@gmail.com>
2023-06-27 13:40:43 -0700

    Add position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -54,7 +54,7 @@ class Square:
             print()
         else:
             for a in range(self.__position[1]):
-                    print("")
+                print("")
             for i in range(self.__size):
                 for k in range(self.__position[0]):
                     print(" ", end="")
