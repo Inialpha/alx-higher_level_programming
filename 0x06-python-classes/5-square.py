257baf87947522803c989e939383cbf08e1fe557
Initech <inimfonebong001@gmail.com>
2023-06-27 11:52:30 -0700

    Add functionality for printing square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" a class Square that defines a square"""
+
+
+class Square:
+    """A class Square that defines a square"""
+    def __init__(self, size=0):
+        """initializes class"""
+        self.size = size
+
+    @property
+    def size(self):
+        """retives the value of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+
+        """set the value of size"""
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """defines the area of a square"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints the squarex"""
+        if self.__size is 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
