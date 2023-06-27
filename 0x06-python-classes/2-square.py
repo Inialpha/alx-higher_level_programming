0eb4cb6e795597aade74a1c746ea554ab61cce2a
Initech <inimfonebong001@gmail.com>
2023-06-27 10:02:04 -0700

    Write a class Square that defines a square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,10 @@ class Square:
     """A class Square that defines a square"""
     def __init__(self, size=0):
         if type(size) is int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
+            if size >= 0:
                 self.__size = size
+
+            else:
+                raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
