3d715cfc2f3f3e90ed90283a5668cbbfd2b43119
Initech <inimfonebong001@gmail.com>
2023-07-05 03:51:36 -0700

    Update handles zerodivisionerror

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,22 +12,20 @@ def matrix_divided(matrix, div):
     """
     mlen = len(matrix)
     for row in matrix:
-        if type(matrix) != list or type(row) != list:
-            raise TypeError("""matrix must be ia
-                    matrix (list of lists) of integers/floats""")
+        if not isinstance(matrix, list) or not isinstance(row, list):
+            raise TypeError("""matrix must be a matrix """ +
+                    """(list of lists) of integers/floats""")
 
         for num in row:
             if not isinstance(num, (int, float)):
-                raise TypeError("""matrix must be a
-                                matrix (list of lists) of integers/floats""")
+                raise TypeError("""matrix must be a matrix """ + """(list of lists) of integers/floats""")
 
     for i in range(mlen):
         if i != mlen - 1:
             if len(matrix[i]) != len(matrix[i + 1]):
                 raise TypeError("Each row of the matrix" +
                                 " must have the same size")
-
-    if not isinstance(div, (float, int)):
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
@@ -40,13 +38,3 @@ def matrix_divided(matrix, div):
         new_list.append(r_list)
 
     return new_list
-
-
-"""matrix = [
-    [1, 2, 3],
-    [5, 2, 3],
-    [4, 5, 6]
-]
-
-print(matrix_divided(matrix, 3))
-print(matrix)"""
