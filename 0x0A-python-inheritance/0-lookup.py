1ea880834531fad41deb505d29ab55bede4a196a
Initech <inimfonebong001@gmail.com>
2023-07-10 04:10:37 -0700

    Fix pycodestyle

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-"""module contains function that return the list of available attributes and methods of an object"""
+"""module contains function that return the list of
+available attributes and methods of an object"""
+
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object
+    """function that returns the list of available
+    attributes and methods of an object
     Args: obj(object)
 
     Return: the list of available attributes and methods of an object"""
 
-    return dir(obj);
+    return dir(obj)
