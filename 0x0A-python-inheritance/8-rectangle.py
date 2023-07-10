9e7893b349e82c9dbfe17668bef485158487d9a7
Initech <inimfonebong001@gmail.com>
2023-07-10 09:25:07 -0700

    creat a Rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""modue for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ A clsss 5hat defines geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+
+    def __init__(self, width, height):
+        """initializes the objects"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
