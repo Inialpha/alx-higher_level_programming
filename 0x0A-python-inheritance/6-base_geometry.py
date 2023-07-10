6de21132c76169310ec535e75a75735add78c4e1
Initech <inimfonebong001@gmail.com>
2023-07-10 05:32:31 -0700

    Add a public instance method area(self)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""modue for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ A clsss 5hat defines geometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
