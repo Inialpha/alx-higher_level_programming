f3403ee1868aa3ee0a3e0bc67f4e0adf45bbf97f
Initech <inimfonebong001@gmail.com>
2023-07-10 10:11:37 -0700

    Add __str__ method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""modue for class Square"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """initializes Square objects"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        """return a string rep of obj"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
