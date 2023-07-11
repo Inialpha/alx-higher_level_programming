754fc4a45f7d20e0f0655be2f40dd49fa6d542d0
Initech <inimfonebong001@gmail.com>
2023-07-11 04:10:33 -0700

    A function that writes an Object to a text file, using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""modle contians save_to json function"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function writes a json rep to  file"""
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
+
+
