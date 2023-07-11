8035d8cdeafa3a132d5ef6b32bb48628b7f51d5f
Initech <inimfonebong001@gmail.com>
2023-07-11 10:14:32 -0700

    Writes a script that reads stdin line by line and computes metrics

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that reads stdin line by line and computes metrics"""
+
+
+from sys import stdin, stdout
+status_codes = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0}
+i = 0
+f_size = 0
+try:
+    for line in stdin:
+        token = line.split()
+        if len(token) >= 2:
+            a = i
+            if token[-2] in status_codes:
+                status_codes[token[-2]] += 1
+                i += 1
+            try:
+                f_size += int(token[-1])
+                if a == i:
+                    i += 1
+            except FileNotFoundError:
+                if a == i:
+                    continue
+            if i % 10 == 0:
+                print("file size: {:d}".format(f_size))
+                for key, value in sorted(status_codes.items()):
+                    if value:
+                        print("{:s}: {:d}".format(key, value))
+    print("file size: {:d}".format(f_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+
+except KeyboardInterrupt:
+    print("file size: {:d}".format(f_size))
+    for key, value in sorted(status_codes.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+    stdout.flush()
