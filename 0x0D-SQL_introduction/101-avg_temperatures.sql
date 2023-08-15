399100c5848c543e16abd991d7f4a80b969a610d
Initech <inimfonebong001@gmail.com>
2023-08-15 11:31:59 -0700

    Add task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,6 @@
 -- displays the average temperature (Fahrenheit) by city
-SELECT city, AVG(value) AS avg_temp
+SELECT city, ROUND(AVG(value), 4) AS avg_temp
 	FROM temperatures
-	WHERE month = 7 OR month = 8
 	GROUP BY city
 	ORDER BY avg_temp DESC
-	LIMIT 3;
+
