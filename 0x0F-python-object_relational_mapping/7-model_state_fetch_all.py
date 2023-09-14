b13a3ba17672ae2ab309a877ff7f5253ac9a7305
Initech <inimfonebong001@gmail.com>
2023-09-14 10:19:15 -0700

    Use ORM

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    pw = sys.argv[2]
+    dbase = sys.argv[3]
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(user, pw, dbase))
+
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    states = session.query(State).order_by('id').all()
+    for state in states:
+        print(state.id, state.name)
