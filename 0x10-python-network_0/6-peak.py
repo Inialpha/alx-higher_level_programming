cc140e16ace923300d073728b000bb1c54373e32
initech <inimfonebong@gmail.com>
2023-09-28 14:12:22 -0700

    Add task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module implement a peak finding algorithm"""
+
+def find_peak(list_of_integers):
+    """A peak findung algorithm"""
+
+    lenght = len(list_of_integers)
+    if lenght == 0:
+        return None;
+
+    for n in range(lenght):
+        try:
+            if list_of_integers[n] >= list_of_integers[n - 1] and list_of_integers[n] >= list_of_integers[n + 1]:
+                return list_of_integers[n]
+        except IndexError:
+            pass
