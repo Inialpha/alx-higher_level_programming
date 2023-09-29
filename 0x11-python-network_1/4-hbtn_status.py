47be4932e7a58560c949067f30cb37439969a45c
Initech <inimfonebong001@gmail.com>
2023-09-29 07:10:24 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
-    print("\t- conent: {}".format(req.text))
+    print("\t- content: {}".format(req.text))
