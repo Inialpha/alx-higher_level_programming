3e45df203ba6e370e0c3b7e2709394708a29fa19
inialpha <inimfonebong001@gmail.com>
2023-10-24 05:38:45 -0700

    Add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+request(url, function (err, response, body) {
+  if (err) {
+    // pass
+  }
+  for (const film of JSON.parse(body).results) {
+    const characters = film.characters;
+    for (const c of characters) {
+      if (c.endsWith('/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
