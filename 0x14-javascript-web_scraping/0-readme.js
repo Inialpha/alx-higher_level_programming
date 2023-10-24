fa15d23982376c569d9e49d3b3bf35eabc5e499f
inialpha <inimfonebong001@gmail.com>
2023-10-24 03:29:26 -0700

    Add a readme file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const args = process.argv;
 const file = args[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
