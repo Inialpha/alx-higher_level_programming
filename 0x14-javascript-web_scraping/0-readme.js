13fdf3884d46f145ce1eeb870a2f8d01397360bc
Initech <inimfonebong001@gmail.com>
2023-10-24 03:16:24 -0700

    Add files

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+const file = args[2];
+
+fs.readFile(file, 'utf8', function (err, data) {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
+});
