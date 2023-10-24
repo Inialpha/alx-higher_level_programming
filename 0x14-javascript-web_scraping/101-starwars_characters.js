966fa1c23eb1fc5a24bc6e1fd52cb8b7cf18db26
inialpha <inimfonebong001@gmail.com>
2023-10-24 07:16:50 -0700

    aadd task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, function (err, response, body) {
+  if (err) {
+    // pass
+  }
+
+  const characters = JSON.parse(body).characters;
+  for (let index = 0; index < characters.length; index++) {
+    request(characters[index], function (err, response, body) {
+      if (err) {
+        //
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
