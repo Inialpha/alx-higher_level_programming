b9946f0116756358d667879ca60a59e2c7807c35
inialpha <inimfonebong001@gmail.com>
2023-10-24 06:32:11 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    // pass
+  }
+  const tasks = JSON.parse(body);
+  const completed = {};
+  for (const task of tasks) {
+    if (task.completed && completed[task.userId] === undefined) {
+      completed[task.userId] = 1;
+    } else if (task.completed) {
+      completed[task.userId]++;
+    }
+  }
+
+  console.log(completed);
+});
