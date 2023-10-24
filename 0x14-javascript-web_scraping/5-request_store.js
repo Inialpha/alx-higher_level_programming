4f5da70c0bad5cd10ac2b4a2516a09c43c4ef14d
inialpha <inimfonebong001@gmail.com>
2023-10-24 06:01:03 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const file = process.argv[3];
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    // pass
+  }
+  fs.writeFile(file, body, 'utf8', function (err) {
+    if (err) {
+      // pass
+    }
+  });
+});
